df7d4942bf3a07c2f327b561e99a1364b584ac18
unknown <you@example.com>
2024-07-16 23:55:37 +0300

    done task 6

--- js/task-6.js
+++ js/task-6.js
@@ -2,4 +2,35 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
+  }
+
+const controlsDiv = document.getElementById('controls');
+const boxesDiv = document.getElementById('boxes');
+
+let currentAmount = 0;
+
+controlsDiv.querySelector('[data-create]').addEventListener('click', createBoxes);
+controlsDiv.querySelector('[data-destroy]').addEventListener('click', destroyBoxes);
+
+function createBoxes() {
+  const amountInput = controlsDiv.querySelector('input[type="number"]');
+  const amount = parseInt(amountInput.value, 10);
+  if (amount < 1 || amount > 100) return;
+
+  createBoxesInner(amount);
+  amountInput.value = '';
+}
+
+function createBoxesInner(amount) {
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement('div');
+    box.style.width = `${30 + i * 10}px`;
+    box.style.height = `${30 + i * 10}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxesDiv.appendChild(box);
+  }
+}
+
+function destroyBoxes() {
+  boxesDiv.innerHTML = '';
 }
